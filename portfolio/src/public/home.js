c69c1f8851fb70ba1a1c43a3673e8a42f6e1d4f8
Kevin Ly <45864296+k-ly73@users.noreply.github.com>
2020-06-15 18:08:10 -0700

    Added new components

---
+++ portfolio/src/public/home.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import "animate.css/animate.min.css";
+import "../assets/css/stylesheet.css";
+
+export const HomePage = () => (
+    <div>
+        hello
+    </div>
+)
+
