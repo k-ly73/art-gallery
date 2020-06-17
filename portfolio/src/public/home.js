038a97bdd722fbb287452a3b509d64948140f95a
Kevin Ly <45864296+k-ly73@users.noreply.github.com>
2020-06-16 17:39:53 -0700

    Added new images

--- portfolio/src/public/home.js
+++ portfolio/src/public/home.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
-import "animate.css/animate.min.css";
-import "../assets/css/stylesheet.css";
 
 export const HomePage = () => (
-    <div>
-        hello
+    <div class = "bg-city">
+        
     </div>
 )
 
