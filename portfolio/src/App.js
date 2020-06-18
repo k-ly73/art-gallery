f64bb4d750b33a10e9053022c80fb996775df259
Kevin Ly <45864296+k-ly73@users.noreply.github.com>
2020-06-17 22:51:08 -0700

    Added content to homepage and added portfolio files/route

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import { HomePage } from './public/home.js';
+import {HomePage} from './public/home.js';
+import {PortfolioPage} from './public/portfolio.js';
 function App() {
   return (
       <React.Fragment>
         <Router>
           <Switch>
-            <Route exact path ="/" component ={HomePage} />
+            <Route exact path ="/" component = {HomePage} />
+            <Route exact path = "/portfolio" component = {PortfolioPage} />
           </Switch>
         </Router>
       </React.Fragment>
