d9a1bc80ad43125f24427699179a8a51e3b22eeb
Kevin Ly <45864296+k-ly73@users.noreply.github.com>
2020-07-13 13:25:49 -0700

    added carousel effect and enable image onclicks for gallery

--- portfolio/src/components/gallery.js
+++ portfolio/src/components/gallery.js
@@ -1,26 +1,38 @@
-import React, { Component, useState, useCallback } from "react";
+import React, {  useState, useCallback } from "react";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
-
-class PhotoGallery extends Component {
-    render() {
+function PhotoGallery() {
         
-        const [currentImage, setCurrentImage] = useState(0);
-        const [viewerIsOpen, setViewerIsOpen] = useState(false);
-        const openLightbox = useCallback((event, { photo, index }) => {
-            setCurrentImage(index);
-            setViewerIsOpen(true);
-        }, []);
-        return (
-            <div className = "container">
-                <Gallery photos = {photos} />
-                <ModalGateway>
-                    
-                    
-                </ModalGateway>
-            </div>
-        )
-    }    
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+    
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    }
+    return (
+        <div className = "container">
+            <Gallery photos = {photos} onClick ={openLightbox}/>
+            <ModalGateway>
+                {viewerIsOpen? (
+                    <Modal onClose = {closeLightbox}>
+                        <Carousel currentIndex = {currentImage}
+                            views = {photos.map(x => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title
+                            }))}
+                        />
+                    </Modal>
+                ) : null }
+            </ModalGateway>
+        </div>
+    );
+      
 }
 export default PhotoGallery
 
