e32a4e21cf1d5cf1cc9a02257be9b86bad458185
Kevin Ly <45864296+k-ly73@users.noreply.github.com>
2021-02-08 15:22:09 -0800

    Updated App.js to show user application

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,21 +4,15 @@ import HomePage  from './components/home';
 import PortfolioPage from './components/portfolio';
 import AboutPage from './components/about';
 import ContactPage from './components/contact';
-
+import Application from './components/application';
+import UserProvider from './components/userprovider';
 
 class App extends Component {
   render() {
     return (
-        <React.Fragment>
-          <Router>
-            <Switch>
-              <Route exact path ="/" component = {HomePage} />
-              <Route exact path = "/portfolio" component = {PortfolioPage} />
-              <Route exact path = "/about" component = {AboutPage} />
-              <Route exact path = "/contact" component = {ContactPage} />
-            </Switch>
-          </Router>
-        </React.Fragment>
+        <UserProvider>
+          <Application />
+        </UserProvider>
     );
   }
 } export default App;
