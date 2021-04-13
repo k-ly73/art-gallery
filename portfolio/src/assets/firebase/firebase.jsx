b3a471b19c3f22516d6406bc60d25394d6b9ac9c
Kevin Ly <45864296+k-ly73@users.noreply.github.com>
2021-04-13 10:42:19 -0700

    Update firebase.jsx

--- portfolio/src/assets/firebase/firebase.jsx
+++ portfolio/src/assets/firebase/firebase.jsx
@@ -5,13 +5,14 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const app = firebase.initializeApp({  
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDK4l6COTGDZn0ZrLB9S925PuYtghEJTyQ",
+  authDomain: "photoalbum-e6d90.firebaseapp.com",
+  databaseURL: "https://photoalbum-e6d90.firebaseio.com",
+  projectId: "photoalbum-e6d90",
+  storageBucket: "photoalbum-e6d90.appspot.com",
+  messagingSenderId: "156930765186",
+  appId: "1:156930765186:web:ba1a4ea1d0110ab9921f9b",
+  measurementId: "G-LP2FCTNHMW"
 });
 
 const provider = new firebase.auth.GoogleAuthProvider();
